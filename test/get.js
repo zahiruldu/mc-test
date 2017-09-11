dbad45c9fbea296820c85c87c5b9dde03fcab428
zahiruldu <zahirul.arb@gmail.com>
2017-09-11 10:48:33 +0600

    get method tested with ratings

--- test/get.js
+++ test/get.js
@@ -21,7 +21,6 @@ describe('/GET Methods', () => {
                     res.body.should.not.have.property('Message');
                     res.body.Results.should.be.a('array');
                     res.body.Count.should.eql(4);
-                    console.log(res.body)
                     done();
                 });
         });
@@ -32,17 +31,16 @@ describe('/GET Methods', () => {
     describe('/vehicles Get Method response with Rating information', () => {
         it('Getting car information with crash Rating', (done) => {
             chai.request(server)
-                .get('/vehicles/2015/Audi/A3?withRating=true')
+                .get('/vehicles/2015/Audi/A3')
+                .query({ withRating: 'true' })
                 .end((err, res) => {
                     res.should.have.status(200);
                     res.body.should.be.a('object');
                     res.body.should.have.property('Count');
                     res.body.should.have.property('Results');
                     res.body.should.not.have.property('Message');
-                    // res.body.should.have.property('CrashRating');
                     res.body.Results.should.be.a('array');
-                    //res.body.Count.should.eql(4);
-                    console.log(res.body)
+                    res.body.Results[0].should.have.property('CrashRating');
                     done();
                 });
         });
@@ -51,17 +49,34 @@ describe('/GET Methods', () => {
     describe('/vehicles Get Method response without Rating information', () => {
         it('Getting car information without crash Rating', (done) => {
             chai.request(server)
-                .get('/vehicles/2015/Audi/A3?withRating=false')
+                .get('/vehicles/2015/Audi/A3')
+                .query({ withRating: 'false' })
                 .end((err, res) => {
                     res.should.have.status(200);
                     res.body.should.be.a('object');
                     res.body.should.have.property('Count');
                     res.body.should.have.property('Results');
                     res.body.should.not.have.property('Message');
-                    //res.body.should.not.have.property('CrashRating');
                     res.body.Results.should.be.a('array');
-                    //res.body.Count.should.eql(4);
-                    console.log(res.body)
+                    res.body.Results[0].should.not.have.property('CrashRating');
+                    done();
+                });
+        });
+    });
+
+    describe('/vehicles Get Method response with rating as banana query information', () => {
+        it('Getting car information with crash Rating for banana query', (done) => {
+            chai.request(server)
+                .get('/vehicles/2015/Audi/A3')
+                .query({ withRating: 'banana' })
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('Count');
+                    res.body.should.have.property('Results');
+                    res.body.should.not.have.property('Message');
+                    res.body.Results.should.be.a('array');
+                    res.body.Results[0].should.have.property('CrashRating');
                     done();
                 });
         });
